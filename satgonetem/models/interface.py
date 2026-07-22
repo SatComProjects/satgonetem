4f0b77bb15bc4bcba3fffa61d11cb6e47e25cfd6
Louis Barbier <louis.barbier@isae-supaero.fr>
2026-07-22 17:46:30 +0200

    Using Mac Address cache system

--- satgonetem/models/interface.py
+++ satgonetem/models/interface.py
@@ -17,6 +17,7 @@ class Interface:
 
         self.type: str = iface_type  # Type of interface, e.g., GndLink or ISL
         self.delay: int = 0
+        self.mac_address: str | None = None
 
     def set_ip(self, ip_address: str) -> None:
         """
@@ -68,3 +69,9 @@ class Interface:
             )
             self.set_ip(local_ip)
             self.peer.set_ip(peer_ip)
+
+    def get_mac_address(self):
+        return self.mac_address
+
+    def set_mac_address(self, src_mac_address):
+        self.mac_address = src_mac_address
